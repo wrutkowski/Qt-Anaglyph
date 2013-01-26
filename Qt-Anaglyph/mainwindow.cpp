a185fd213592f19b6da232e4e6e327a9b716d5b7
ppalus <patrykpalus@gmail.com>
2013-01-26 20:18:10 +0100

    skalowanie i wyswitlanie obrazow

--- Qt-Anaglyph/mainwindow.cpp
+++ Qt-Anaglyph/mainwindow.cpp
@@ -18,6 +18,8 @@ void MainWindow::on_pushButton_clicked()
     _leftEyeImage = QFileDialog::getOpenFileName(this, tr("Open Image"), "", tr("Image Files (*.png *.jpeg *.bmp *.jpg)"));
     qDebug() << _leftEyeImage;
     _a.addLeftEyeImage(_leftEyeImage);
+    QImage imgCpy = _a.getLeftEyeImage();
+    ui->label_3->setPixmap(QPixmap::fromImage(imgCpy.scaled(QSize(ui->label_3->width(), ui->label_3->height()))));
 }
 
 void MainWindow::on_pushButton_2_clicked()
@@ -25,4 +27,6 @@ void MainWindow::on_pushButton_2_clicked()
     _rightEyeImage = QFileDialog::getOpenFileName(this, tr("Open Image"), "", tr("Image Files (*.png *.jpeg *.bmp *.jpg)"));
     qDebug() << _rightEyeImage;
     _a.addRightEyeImage(_rightEyeImage);
+    QImage imgCpy = _a.getRigthEyeImage();
+    ui->label_2->setPixmap(QPixmap::fromImage(imgCpy.scaled(QSize(ui->label_2->width(), ui->label_2->height()))));
 }
