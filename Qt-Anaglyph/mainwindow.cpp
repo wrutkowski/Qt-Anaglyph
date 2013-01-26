7a2c68b01eee3816cc4510dcb3784328f2b0784a
ppalus <patrykpalus@gmail.com>
2013-01-26 21:27:14 +0100

    poprawione skalowanie

--- Qt-Anaglyph/mainwindow.cpp
+++ Qt-Anaglyph/mainwindow.cpp
@@ -16,17 +16,25 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     _leftEyeImage = QFileDialog::getOpenFileName(this, tr("Open Image"), "", tr("Image Files (*.png *.jpeg *.bmp *.jpg)"));
-    qDebug() << _leftEyeImage;
     _a.addLeftEyeImage(_leftEyeImage);
     QImage imgCpy = _a.getLeftEyeImage();
-    ui->label_3->setPixmap(QPixmap::fromImage(imgCpy.scaled(QSize(ui->label_3->width(), ui->label_3->height()))));
+    float wProp = 1.0f;
+    if(imgCpy.width() > ui->label_3->width())
+        wProp = imgCpy.width() / ui->label_3->width();
+    else if(imgCpy.width() < ui->label_3->width())
+        wProp = ui->label_3->width() / imgCpy.width();
+    ui->label_3->setPixmap(QPixmap::fromImage(imgCpy.scaled(QSize(ui->label_3->width()/wProp, ui->label_3->height()))));
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
     _rightEyeImage = QFileDialog::getOpenFileName(this, tr("Open Image"), "", tr("Image Files (*.png *.jpeg *.bmp *.jpg)"));
-    qDebug() << _rightEyeImage;
     _a.addRightEyeImage(_rightEyeImage);
     QImage imgCpy = _a.getRigthEyeImage();
-    ui->label_2->setPixmap(QPixmap::fromImage(imgCpy.scaled(QSize(ui->label_2->width(), ui->label_2->height()))));
+    float wProp = 1.0f;
+    if(imgCpy.width() > ui->label_2->width())
+        wProp = imgCpy.width() / ui->label_2->width();
+    else if(imgCpy.width() < ui->label_2->width())
+        wProp = ui->label_2->width() / imgCpy.width();
+    ui->label_2->setPixmap(QPixmap::fromImage(imgCpy.scaled(QSize(ui->label_2->width()/wProp, ui->label_2->height()))));
 }
